b0a61d56147a9c637f9231369d6743b39d0d7823
Rahul <merahul220@gmail.com>
2022-07-11 23:19:16 +0530

    Thrid commit

--- src/test/java/TestCase/Check_Out.java
+++ src/test/java/TestCase/Check_Out.java
@@ -3,25 +3,36 @@ package TestCase;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import resources.Base;
 
 public class Check_Out extends Base {
 	
 	WebDriver diver;
+	public Check_Out() {
+		super();
+	}
+
+	@BeforeMethod
 	public void setup() throws IOException
 	{
 		driver=intializeBrowser();
-		prop.getProperty(browserName);
+		driver.get(prop.getProperty("url"));
 	}
+	
+	@Test
 	public void checkOut()
 	{
 		
 	}
 	
+	@AfterMethod
 	public void tearDown()
 	{
-		
+		//driver.quit();
 	}
 
 }
